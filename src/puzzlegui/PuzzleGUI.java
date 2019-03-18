eb7d5aa113d7814287a858b6e67bce7bf6151b00
Recep Karaca <recepkaraca@yandex.com>
2019-03-18 15:23:37 +0300

    Score and high score added.

--- src/puzzlegui/PuzzleGUI.java
+++ src/puzzlegui/PuzzleGUI.java
@@ -55,6 +55,9 @@ public class PuzzleGUI extends javax.swing.JFrame {
         this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
         setFilePath(s);
         readImage();
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setEnabled(false);
+        }
         scoreClass = new Score();
         scoreClass.setScore(0);
         scoreClass.readFile();
@@ -90,7 +93,6 @@ public class PuzzleGUI extends javax.swing.JFrame {
         jButton16 = new javax.swing.JButton();
         mixUp = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jButton17 = new javax.swing.JButton();
         usernamelb = new javax.swing.JLabel();
         scorelb = new javax.swing.JLabel();
         highscorelb = new javax.swing.JLabel();
@@ -201,13 +203,6 @@ public class PuzzleGUI extends javax.swing.JFrame {
             }
         });
 
-        jButton17.setText("jButton17");
-        jButton17.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton17ActionPerformed(evt);
-            }
-        });
-
         usernamelb.setFont(new java.awt.Font("Tahoma", 0, 15)); // NOI18N
         usernamelb.setText("jLabel2");
 
@@ -255,10 +250,7 @@ public class PuzzleGUI extends javax.swing.JFrame {
                                 .addComponent(jButton14, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jButton15, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(mixUp, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton17, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(mixUp, javax.swing.GroupLayout.PREFERRED_SIZE, 160, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(usernamelb)
@@ -310,8 +302,7 @@ public class PuzzleGUI extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(scorelb)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(highscorelb))
-                    .addComponent(jButton17, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addComponent(highscorelb))))
         );
 
         pack();
@@ -482,6 +473,11 @@ public class PuzzleGUI extends javax.swing.JFrame {
         for (int i = 0; i < buttons.length; i++) {
             buttons[i].setEnabled(false);
         }
+        if(scoreClass.getScore() > scoreClass.getHighScore()){
+            highscorelb.setText("High Score: " + scoreClass.getScore());
+        }else{
+            highscorelb.setText("High Score: " + scoreClass.getHighScore());
+        }
         scoreClass.writeFile();
     }
 
@@ -501,9 +497,9 @@ public class PuzzleGUI extends javax.swing.JFrame {
         } else {
             double addValue = 0;
             if (failture < 12) {
-                addValue = 6.25 - (failture * 0.5);
+                addValue = (sumOfCorrectParts * 6.25) - (failture * 0.5);
             } else {
-                addValue = 6.25 - (5.75);
+                addValue = (sumOfCorrectParts * 6.25) - (5.75);
             }
             scoreClass.plusScore(addValue);
         }
@@ -514,9 +510,12 @@ public class PuzzleGUI extends javax.swing.JFrame {
         }
         System.out.println("Failture: " + failture);
         System.out.println("Score: " + scoreClass.getScore());
-        scoreClass.highScoreCompare();
         scorelb.setText("Score: " + scoreClass.getScore());
-        highscorelb.setText("High Score: " + scoreClass.getHighScore());
+        if(scoreClass.getScore() > scoreClass.getHighScore()){
+            highscorelb.setText("High Score: " + scoreClass.getScore());
+        }else{
+            highscorelb.setText("High Score: " + scoreClass.getHighScore());
+        }
         if (isSolved()) {
             finalizePuzzle();
         }
@@ -560,9 +559,15 @@ public class PuzzleGUI extends javax.swing.JFrame {
         System.out.println("-------");
         scoreClass.setScore(0);
         scoreClass.plusScore(totalCorrectParts() * 6.25);
-        scoreClass.highScoreCompare();
         scorelb.setText("Score: " + scoreClass.getScore());
-        highscorelb.setText("High Score: " + scoreClass.getHighScore());
+        if(scoreClass.getScore() > scoreClass.getHighScore()){
+            highscorelb.setText("High Score: " + scoreClass.getScore());
+        }else{
+            highscorelb.setText("High Score: " + scoreClass.getHighScore());
+        }
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i].setEnabled(true);
+        }
         if(isSolved())
             finalizePuzzle();
         //jButton1.setIcon(new ImageIcon(slices[0]));
@@ -809,55 +814,6 @@ public class PuzzleGUI extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jButton16ActionPerformed
 
-    private void jButton17ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton17ActionPerformed
-        int width = 640;
-        int height = 640;
-        BufferedImage image2 = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        File f2 = new File("output.jpg"); //image file path
-        try {
-            image2 = ImageIO.read(f2);
-        } catch (IOException ex) {
-            Logger.getLogger(PuzzleGUI.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println(image2.getWidth() + " " + image2.getHeight());
-        int w = 160;
-        int h = 160;
-        final int rows = 4;
-        final int cols = 4;
-        BufferedImage[] slices = new BufferedImage[rows * cols];
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                slices[(i * cols) + j] = image2.getSubimage(j * w, i * h, w, h);
-            }
-        }
-
-        JButton[] buttons = new JButton[16];
-        buttons[0] = jButton1;
-        buttons[1] = jButton2;
-        buttons[2] = jButton3;
-        buttons[3] = jButton4;
-        buttons[4] = jButton5;
-        buttons[5] = jButton6;
-        buttons[6] = jButton7;
-        buttons[7] = jButton8;
-        buttons[8] = jButton9;
-        buttons[9] = jButton10;
-        buttons[10] = jButton11;
-        buttons[11] = jButton12;
-        buttons[12] = jButton13;
-        buttons[13] = jButton14;
-        buttons[14] = jButton15;
-        buttons[15] = jButton16;
-
-        for (int i = 0; i < slices.length; i++) {
-            originalBtnImg.put(buttons[i], slices[i]);
-            buttons[i].setIcon(new ImageIcon(slices[i]));
-        }
-        if(isSolved())
-            finalizePuzzle();
-    }//GEN-LAST:event_jButton17ActionPerformed
-
     private static BufferedImage toBufferedImage(Image img) {
         int width = img.getWidth(null);
         int height = img.getHeight(null);
@@ -882,7 +838,6 @@ public class PuzzleGUI extends javax.swing.JFrame {
     private javax.swing.JButton jButton14;
     private javax.swing.JButton jButton15;
     private javax.swing.JButton jButton16;
-    private javax.swing.JButton jButton17;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
